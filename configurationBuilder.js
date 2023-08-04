917284e95939dd7ea57c229c343883fcf265dbd4
juan <jguardiolagarcia@gmail.com>
2023-08-04 14:33:33 -0300

    set configurationBuilder.js

---
+++ configurationBuilder.js
@@ -0,0 +1,22 @@
+/****************************************************
+ Configuration builder
+
+ The configuration object should be built in order to configure the package dependencies
+
+ ****************************************************/
+
+let configurationBuilder = function (config) {
+    let newConfig = {
+        oauth: {}
+    };
+    let oauthConfig = newConfig.oauth
+    oauthConfig.id = config.id;
+    oauthConfig.authUrl = config.authUrl;
+    oauthConfig.accessTokenUrl = config.accessTokenUrl;
+    oauthConfig.clientId = config.clientId;
+    oauthConfig.clientSecret = config.clientSecret;
+    oauthConfig.scope = config.scope;
+    oauthConfig.state = config.state;
+    oauthConfig.oauthCallback = config.oauthCallback;
+    return newConfig;
+}
